d72bd465caa3d313b4664ea1492854c8e63fe470
Alex Galuza <0.chto.a.cho.0@gmail.com>
2024-01-10 18:19:49 +0300

    correct task2

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -2,9 +2,11 @@
 int[] arr = new int[n];
 int i = 0;
 
-while (i<n){
-   arr[i] = i + 1;
- //    Console.Write(arr[i]);
- //    Console.Write(' ');
-   Console.Write($"{arr[i]} ");
-   i = i + 1;}
+while (i < arr.Length)
+{
+  arr[i] = i + 1;
+  //    Console.Write(arr[i]);
+  //    Console.Write(' ');
+  Console.Write($"{arr[i]} ");
+  i = i + 1;
+}
