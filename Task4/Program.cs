5fe8281baeccc3622e54145b875d44660ff849f9
Alex Galuza <0.chto.a.cho.0@gmail.com>
2024-01-16 23:12:59 +0300

    lesson 1: task4

---
+++ Task4/Program.cs
@@ -0,0 +1,36 @@
+﻿int n = 5;
+int[] array = {1,4,3,8,10};
+int i = 0;
+int max = array[0];
+while(i < n)
+{
+    if (array[i] > max)
+    {
+        max = array[i];
+    }
+    i = i + 1;
+}
+
+Console.WriteLine(max);
+
+max = array[0];
+for(int j = 0; j<n; j++)
+{
+     if (array[j] > max)
+    {
+        max = array[j];
+    }
+    j = j + 1;
+}
+
+Console.WriteLine(max);
+
+max =array[0];
+foreach(int e in array)
+{
+      if (e > max)
+    {
+        max = e;
+    } 
+}
+Console.WriteLine(max);
